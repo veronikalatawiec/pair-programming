62725e362e801862c0737e7b1260763f985e486b
christinz <christinyzhong@gmail.com>
2025-02-27 11:10:43 -0500

    install cors

--- photobooth-api/index.js
+++ photobooth-api/index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import cors from "cors";
 import fs from "fs";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
+
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = 5050;
+
+// stripRoutes
+app.post();
