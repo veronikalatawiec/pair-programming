7f58291e802045cfe7bdc66639a070551f1e68fc
Rabia-Tahir542 <Biatahir2002@gmail.com>
2025-02-27 13:41:11 -0500

    photobooth code

---
+++ photobooth-app/src/pages/PhotoBooth/PhotoBooth.jsx
@@ -0,0 +1,12 @@
+import Webcam from "react-webcam";
+import {useref} from 'react';
+
+function PhotoBooth() {
+  return ( 
+  <div className = "PhotoBooth">
+    React Webcam 
+    <Webcam/> 
+  </div>
+  );
+}
+export default PhotoBooth;
